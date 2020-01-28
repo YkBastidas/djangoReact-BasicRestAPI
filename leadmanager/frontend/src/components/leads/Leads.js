ebaedc55d18d04d5f4372c91453743e5a5c17ca7
Yorfrank Bastidas <ykbastidasc@gmail.com>
2020-01-28 01:02:00 -0400

    Show every registered user in a table at main page
    The action "GET_USERS" make a request to the database using axios and
    set the response to the payload that save the response in the initial
    state.

--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,13 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getUsers } from "../../actions/leads";
 
 export class Leads extends Component {
+  static PropTypes = {
+    users: PropTypes.array.isRequired
+  };
+
+  componentDidMount() {
+    this.props.getUsers();
+  }
+
   render() {
     return (
-      <div>
-        <h1> Leads </h1>
-      </div>
-    )
+      <Fragment>
+        <h2>Usuarios</h2>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nombre de Usuario</th>
+              <th>Fecha de Creación</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.users.map(user => (
+              <tr key={user.user_id}>
+                <td>{user.user_id}</td>
+                <td>{user.user_name}</td>
+                <td>{user.user_created_at}</td>
+                <td>
+                  <button className="btn btn-danger btn-sm"> Delete </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Fragment>
+    );
   }
 }
 
-export default Leads
+const mapStateToProps = state => ({
+  users: state.leads.users
+});
+
+export default connect(mapStateToProps, { getUsers })(Leads);
