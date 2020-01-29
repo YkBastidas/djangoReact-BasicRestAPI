06a69bb210d54ae3945c344308a377888b03c45a
Yorfrank Bastidas <ykbastidasc@gmail.com>
2020-01-29 10:02:41 -0400

    Create reset form button

--- leadmanager/frontend/src/components/createUser/Form.js
+++ leadmanager/frontend/src/components/createUser/Form.js
@@ -35,6 +35,15 @@ export class Form extends Component {
     });
   };
 
+  onReset = e => {
+    e.preventDefault();
+    this.setState({
+      username: "",
+      password: "",
+      role_id: ""
+    });
+  };
+
   componentDidMount() {
     this.props.getRoles();
   }
@@ -44,54 +53,66 @@ export class Form extends Component {
     return (
       <div className="card card-body mt-4 mb-4">
         <h2>Crear Usuario</h2>
-        <form onSubmit={this.onSubmit}>
-          <div className="form-group">
-            <label>Nombre de Usuario</label>
-            <input
-              type="text"
-              className="form-control"
-              name="username"
-              onChange={this.onChange}
-              value={username}
-              maxLength="30"
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label>Contraseña</label>
-            <input
-              type="password"
-              className="form-control"
-              name="password"
-              onChange={this.onChange}
-              value={password}
-              maxLength="20"
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label> Rol </label>
-            <select
-              className="custom-select"
-              name="role_id"
-              required
-              onChange={this.onChange}
-              value={role_id}
-            >
-              <option value="">Seleccione...</option>
-              {this.props.roles.map(role => (
-                <option key={role.role_id} value={role.role_id}>
-                  {" "}
-                  {role.role_name}{" "}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="form-group">
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </div>
+        <form onSubmit={this.onSubmit} onReset={this.onReset}>
+          <fieldset>
+            <legend> Cuenta </legend>
+            <div className="form-group row">
+              <div className="col-sm-4">
+                <label>Nombre de Usuario</label>
+                <input
+                  type="text"
+                  className="form-control bg-light"
+                  name="username"
+                  onChange={this.onChange}
+                  value={username}
+                  maxLength="30"
+                  required
+                />
+              </div>
+              <div className="col-sm-3">
+                <label>Contraseña</label>
+                <input
+                  type="password"
+                  className="form-control bg-light"
+                  name="password"
+                  onChange={this.onChange}
+                  value={password}
+                  maxLength="20"
+                  required
+                />
+              </div>
+              <div className="col-sm-4">
+                <label> Rol </label>
+                <select
+                  className="custom-select bg-light"
+                  name="role_id"
+                  required
+                  onChange={this.onChange}
+                  value={role_id}
+                >
+                  <option value="">Seleccione...</option>
+                  {this.props.roles.map(role => (
+                    <option key={role.role_id} value={role.role_id}>
+                      {" "}
+                      {role.role_name}{" "}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+            <div className="form-group row">
+              <div className="col-sm-2">
+                <button type="submit" className="btn btn-primary ">
+                  Registrarse
+                </button>
+              </div>
+              <div className="col-sm-1">
+                <button type="reset" className="btn btn-warning">
+                  Borrar
+                </button>
+              </div>
+            </div>
+          </fieldset>
         </form>
       </div>
     );
